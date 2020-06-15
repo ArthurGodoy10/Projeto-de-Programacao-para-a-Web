7b397af356ca9b443b423bd5b991d2e40fc96869
Arthur Couto <2014arthur@gmail.com>
2020-06-15 02:38:41 +0100

    Create index.php
    Criei o index do meu trabalho, que é a minha pagina inicial.

---
+++ Trabalho_PHP/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div id="corpo-form">
+        <h1>Entrar</h1>
+        <form method="POST">
+            <input type="email" name="email" placeholder="Usuário">
+            <input type="password" name="senha" placeholder="Senha">
+            <input type="submit" value="ACESSAR">
+            <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se</strong>
+        </form>
+    </div>
+
+
+</body>
+</html>
