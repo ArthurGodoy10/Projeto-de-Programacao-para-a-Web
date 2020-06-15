df5806226140639dba8fb067da58dc40a5ffc699
Arthur Couto <2014arthur@gmail.com>
2020-06-15 03:05:35 +0100

    Update usuarios.php
    Criação de algumas funções no ficheiro usuarios.

--- Trabalho_PHP/Classes/usuarios.php
+++ Trabalho_PHP/Classes/usuarios.php
@@ -1,4 +1,47 @@
 <?php
 
+Class Usuario
+{
+    private $pdo;
+    public $msgErro = "";
+
+    public function conectar($nome, $host, $usuario, $senha)
+    {
+        global $pdo;
+        try {
+            $pdo = new PDO("mysql:dbname=".$nome.";host=".$host,$usuario,$senha);
+        } catch (PDOException $e) {
+            $msgErro = $e->getMessage();
+        }
+    }
+
+    public function cadastrar($nome, $email, $senha)
+    {
+        global $pdo;
+        //verificar se já existe o email cadastrado
+        $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e");
+        $sql->bindValue(":e",$email);
+        $sql->execute();
+        if($sql->rowCount() > 0)
+        {
+            return false;// ja esta cadatrada
+        }
+        else
+        {
+             //caso não, cadatrar
+             $sql = $pdo->prepare("INSERT INTO usuarios (nome, email, senha) VALUES (:n, :e, :s)");
+             $sql->bindValue(":n",$nome);
+             $sql->bindValue(":e",$email);
+             $sql->bindValue(":s",$senha);
+             $sql->execute();
+             return true;
+        }
+
+    }
+    public function logar()
+    {
+        global $pdo;
+    }
+}
 
 ?>
