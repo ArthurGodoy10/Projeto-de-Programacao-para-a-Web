2c5c99aedb3b790ef0dec561d7b732b853ee0096
Arthur Couto <2014arthur@gmail.com>
2020-06-15 02:49:11 +0100

    Create cadastrar.php
    Crianda a pagina de cadastro.

---
+++ Trabalho_PHP/cadastrar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Cadastrar</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div id="corpo-form">
+        <h1>Cadastrar</h1>
+        <form method="POST">
+            <input type="text" name="nome" placeholder="Nome Completo" maxlength="30">
+            <input type="email" name="email" placeholder="Username" maxlength="40">
+            <input type="password" name="senha" placeholder="Senha" maxlength="15">
+            <input type="password" name="confsenha" placeholder="Confirmar Senha" maxlength="15">
+            <input type="submit" value="ACESSAR">
+
+        </form>
+    </div>
+</body>
+
+</html>
