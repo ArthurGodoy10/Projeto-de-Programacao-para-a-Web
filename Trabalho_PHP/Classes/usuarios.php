9e3c517014413a1e546317ae7c6ae4a2b85a32d0
Arthur Couto <2014arthur@gmail.com>
2020-06-15 03:49:32 +0100

    Update usuarios.php
    Adicionada a funcao logar comleta

--- Trabalho_PHP/Classes/usuarios.php
+++ Trabalho_PHP/Classes/usuarios.php
@@ -41,7 +41,22 @@ Class Usuario
     public function logar()
     {
         global $pdo;
+        //verificar se o email e senha estao casdastrados, se sim 
+        $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e AND senha = :s");
+        $sql->bindValue(":e",$email);
+        $sql->bindValue(":s",$senha);
+        $sql->execute();
+        if($sql->rowCount() > 0)
+        {
+            //entar no sistema (sessao)
+            $dado = $sql->fetch();
+            session_start();
+            $_SESSION['id_usuario'] = $dado['id_usuario'];
+            return true; //llogado com sucesso
+        }
+        else
+        {
+            return false; // nao foi possivel logar
+        }
     }
 }
-
-?>
