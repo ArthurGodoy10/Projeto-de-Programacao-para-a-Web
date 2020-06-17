c3e307e67aeb6d9d101a5694716545e6479ffb14
Arthur Couto <2014arthur@gmail.com>
2020-06-17 15:11:04 +0100

    Update cadastrar.php
    Atualização da pagina de cadastro, com códigos em php, incluindo a conexão a  basa de dados.

--- Trabalho_PHP/cadastrar.php
+++ Trabalho_PHP/cadastrar.php
@@ -1,14 +1,18 @@
+<?php
+include('Classes/usuarios.php');
+$u = new Usuario;
+?>
+
 <!DOCTYPE html>
 <html lang="pt">
 
 <head>
     <meta charset="UTF-8">
-
     <title>Cadastrar</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
-<body>
+<body id="cadastrar">
     <div id="corpo-form">
         <h1>Cadastrar</h1>
         <form method="POST">
@@ -16,10 +20,60 @@
             <input type="email" name="email" placeholder="Username" maxlength="40">
             <input type="password" name="senha" placeholder="Senha" maxlength="15">
             <input type="password" name="confsenha" placeholder="Confirmar Senha" maxlength="15">
-            <input type="submit" value="ACESSAR">
-
+            <input type="submit" value="Cadastrar">
         </form>
     </div>
+    <?php
+    //verificar se clicou no botao
+    if (isset($_POST['nome'])) {
+        $nome = addslashes($_POST['nome']);
+        $email = addslashes($_POST['email']);
+        $senha = addslashes($_POST['senha']);
+        $confirmarSenha = addslashes($_POST['confsenha']);
+        //verificar se esta preenchido
+
+        if (!empty($nome) && !empty($email) && !empty($senha) && !empty($confirmarSenha)) {
+            $u->conectar("localhost", "projeto_php", "root", "suasenha");
+            if ($u->msgErro == "") //se esta tudo ok
+            {
+                if ($senha == $confirmarSenha) {
+                    if ($u->cadastrar($nome, $email, $senha)) {
+                        ?>
+                        <div id="msg-sucesso">
+                            Cadastrado com sucesso! Acesse para entrar!
+                        </div>
+                        <?php
+                    } else {
+                        ?>
+                        <div class="msg-erro">
+                            Email já Cadastrado!
+                        </div>
+                        <?php
+                    }
+                } else {
+                    ?>
+                    <div class="msg-erro">
+                        Senha e confirmar senha não correspondem!
+                    </div>
+                    <?php
+                }
+            } else {
+                ?>
+                <div class="msg-erro">
+                    <?php echo "Erro: " . $u->msgErro; ?>
+                </div>
+                <?php
+            }
+        } else {
+            ?>
+            <div class="msg-erro">
+                Preencha todos os campos!
+            </div>
+            <?php
+
+        }
+    }
+    ?>
 </body>
 
 </html>
