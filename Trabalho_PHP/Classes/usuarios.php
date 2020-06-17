8cc3f59184afda0c50be60c3bc8707b0ec6f873b
Arthur Couto <2014arthur@gmail.com>
2020-06-17 03:52:44 +0100

    Update usuarios.php
    Atualização da ficheiro usuarios, melhorias de códigos, correção, e novas implementações no código.

--- Trabalho_PHP/Classes/usuarios.php
+++ Trabalho_PHP/Classes/usuarios.php
@@ -1,62 +1,69 @@
 <?php
 
-Class Usuario
-{
-    private $pdo;
-    public $msgErro = "";
-
-    public function conectar($nome, $host, $usuario, $senha)
+    Class Usuario
     {
-        global $pdo;
-        try {
-            $pdo = new PDO("mysql:dbname=".$nome.";host=".$host,$usuario,$senha);
-        } catch (PDOException $e) {
-            $msgErro = $e->getMessage();
-        }
-    }
+        private $pdo;
+        public $msgErro = "";// tudo ok
 
-    public function cadastrar($nome, $email, $senha)
-    {
+        public function conectar($host, $nome, $usuario, $senha)
+        {
         global $pdo;
-        //verificar se já existe o email cadastrado
-        $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e");
-        $sql->bindValue(":e",$email);
-        $sql->execute();
-        if($sql->rowCount() > 0)
-        {
-            return false;// ja esta cadatrada
-        }
-        else
-        {
-             //caso não, cadatrar
-             $sql = $pdo->prepare("INSERT INTO usuarios (nome, email, senha) VALUES (:n, :e, :s)");
-             $sql->bindValue(":n",$nome);
-             $sql->bindValue(":e",$email);
-             $sql->bindValue(":s",$senha);
-             $sql->execute();
-             return true;
+        global $msgErro;
+
+            try 
+            {
+                $pdo = new PDO("mysql:host=".$host.";dbname=".$nome, $usuario, $senha);
+            } 
+            catch (PDOException $e) 
+            {
+                $msgErro = $e->getMessage();
+            }
         }
 
-    }
-    public function logar()
-    {
+        public function cadastrar($nome, $email, $senha)
+        {
         global $pdo;
-        //verificar se o email e senha estao casdastrados, se sim 
-        $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e AND senha = :s");
-        $sql->bindValue(":e",$email);
-        $sql->bindValue(":s",$senha);
-        $sql->execute();
-        if($sql->rowCount() > 0)
-        {
-            //entar no sistema (sessao)
-            $dado = $sql->fetch();
-            session_start();
-            $_SESSION['id_usuario'] = $dado['id_usuario'];
-            return true; //llogado com sucesso
+            //verificar se já existe o email cadastrado
+            $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e");
+            $sql->bindValue(":e",$email);
+            $sql->execute();
+
+            if($sql->rowCount() > 0)
+            {
+                return false;// ja esta cadatrada
+            }
+            else
+            {
+            //caso não, cadatrar
+                $sql = $pdo->prepare("INSERT INTO usuarios (nome, email, senha) VALUES (:n, :e, :s)");
+                $sql->bindValue(":n",$nome);
+                $sql->bindValue(":e",$email);
+                $sql->bindValue(":s",md5($senha));
+                $sql->execute();
+                return true;
+            }
+
         }
-        else
+        public function logar($email, $senha)
         {
-            return false; // nao foi possivel logar
+            global $pdo;
+            //verificar se o email e senha estao casdastrados, se sim 
+            $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e AND senha = :s");
+            $sql->bindValue(":e",$email);
+            $sql->bindValue(":s",md5($senha));
+            $sql->execute();
+
+            if($sql->rowCount() > 0 )
+            {
+                //entar no sistema (sessao)
+                $dado = $sql->fetch();
+                session_start();
+                $_SESSION['id_usuario'] = $dado['id_usuario'];
+                return true; //logado com sucesso
+            }
+            else
+            {
+                return false; // nao foi possivel logar
+            }
         }
     }
-}
