154999d6c724c72012993c77b61420d312232869
Arthur Couto <2014arthur@gmail.com>
2020-06-17 03:50:24 +0100

    Update index.php
    Atualização do index , com novas partes em php, e novo código.

--- Trabalho_PHP/index.php
+++ Trabalho_PHP/index.php
@@ -1,14 +1,16 @@
+<?php
+include('Classes/usuarios.php');
+$u = new Usuario;
+
+?>
 <!DOCTYPE html>
 <html lang="pt">
-
 <head>
     <meta charset="UTF-8">
-
     <title>Login</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-
-<body>
+<body id="login">
     <div id="corpo-form">
         <h1>Entrar</h1>
         <form method="POST">
@@ -18,7 +20,39 @@
             <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se</strong>
         </form>
     </div>
+    <?php
+    if (isset($_POST['email'])) {
+        $email = addslashes($_POST['email']);
+        $senha = addslashes($_POST['senha']);
+        //verificar se esta preenchido
 
-
+        if (!empty($email) && !empty($senha)) {
+            $u->conectar("localhost", "projeto_php", "root", "suasenha");
+            if ($u->msgErro == "") {
+                if ($u->logar($email, $senha)) {
+                    header("location: areaPrivada.php");
+                } else {
+                    ?>
+                    <div class="msg-erro">
+                        Email e/ou senha estão incorretos!
+                    </div>
+                    <?php
+                }
+            } else {
+                ?>
+                <div class="msg-erro">
+                    <?php echo "Erro: " . $u->msgErro; ?>
+                </div>
+                <?php
+            }
+        } else {
+            ?>
+            <div class="msg-erro">
+                Preencher todos os campos!
+            </div>
+            <?php
+        }
+    }
+?>
 </body>
 </html>
