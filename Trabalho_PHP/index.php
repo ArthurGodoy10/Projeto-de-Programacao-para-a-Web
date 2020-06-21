02b423df068c28f03f2f7543e7d2c16e94b6b21c
Arthur Couto <2014arthur@gmail.com>
2020-06-21 05:16:46 +0100

    Update index.php
    Atualização feita no no index, onde foi modificado e melhorado.

--- Trabalho_PHP/index.php
+++ Trabalho_PHP/index.php
@@ -1,58 +1,52 @@
 <?php
-include('Classes/usuarios.php');
-$u = new Usuario;
+session_start();
+session_destroy();
 
+$past = time() - 3600;
+foreach ($_COOKIE as $key => $value) {
+    setcookie($key, $value, $past, '/');
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css" />
 </head>
+
 <body id="login">
     <div id="corpo-form">
         <h1>Entrar</h1>
         <form method="POST">
-            <input type="email" name="email" placeholder="Usuário">
-            <input type="password" name="senha" placeholder="Senha">
+            <input type="email" name="email" placeholder="Usuário" required>
+            <input type="password" name="senha" placeholder="Senha" required>
             <input type="submit" value="ACESSAR">
-            <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se</strong>
+            <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se</strong></a>
         </form>
     </div>
     <?php
+    include('Classes/usuarios.php');
     if (isset($_POST['email'])) {
         $email = addslashes($_POST['email']);
         $senha = addslashes($_POST['senha']);
         //verificar se esta preenchido
 
         if (!empty($email) && !empty($senha)) {
-            $u->conectar("localhost", "projeto_php", "root", "suasenha");
-            if ($u->msgErro == "") {
-                if ($u->logar($email, $senha)) {
-                    header("location: areaPrivada.php");
-                } else {
-                    ?>
-                    <div class="msg-erro">
-                        Email e/ou senha estão incorretos!
-                    </div>
-                    <?php
-                }
+            if (logar($email, $senha)) {
+                header("location: loginControler.php");
             } else {
-                ?>
+    ?>
                 <div class="msg-erro">
-                    <?php echo "Erro: " . $u->msgErro; ?>
+                    Email e/ou senha estão incorretos!
                 </div>
-                <?php
+    <?php
             }
-        } else {
-            ?>
-            <div class="msg-erro">
-                Preencher todos os campos!
-            </div>
-            <?php
         }
     }
-?>
+    ?>
 </body>
+
 </html>
