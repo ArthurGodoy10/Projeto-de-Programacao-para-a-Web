087c963c9e40c9d14a7177a50f8d489f16b9fffe
Arthur Couto <2014arthur@gmail.com>
2020-06-21 05:25:50 +0100

    Create areaPrivada.php
    Inclusão do html base do jogo.

---
+++ Trabalho_PHP/areaPrivada.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Jump Game</title>
+    <link rel="stylesheet" href="css/index.css" />
+</head>
+<body>
+    <h1>Welcome</h1>
+    <a href="sair.php"> Sair </a>
+    <span id="span">fsfsd</span>
+    <script src="JS/index.js"></script>
+</body>
+</html>
