f92c6e511c67a6e131514c91cd3f7532d2a81859
Arthur Couto <2014arthur@gmail.com>
2020-06-23 01:12:40 +0100

    Update areaPrivada.php
    Mudanças na areaPrivada, adicionando uma segurança no ficheiro.

--- Trabalho_PHP/areaPrivada.php
+++ Trabalho_PHP/areaPrivada.php
@@ -1,13 +1,23 @@
+<?php
+session_start();
+if (!isset($_SESSION['id_usuario'])) {
+    header("location: index.php");
+    exit;
+}
+?>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>Jump Game</title>
     <link rel="stylesheet" href="css/index.css" />
 </head>
+
 <body>
     <h1>Welcome</h1>
     <a href="sair.php"> Sair </a>
     <span id="span">fsfsd</span>
     <script src="JS/index.js"></script>
 </body>
+
 </html>
