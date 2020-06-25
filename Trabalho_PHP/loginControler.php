44ae12fe76e1ec58e316436d0e0f9dee9178bf6e
Arthur Couto <2014arthur@gmail.com>
2020-06-25 16:41:38 +0100

    Update loginControler.php

--- Trabalho_PHP/loginControler.php
+++ Trabalho_PHP/loginControler.php
@@ -2,7 +2,8 @@
 session_start();
 if (isset($_SESSION['id_usuario'])) {
     header("location: areaPrivada.php");
-} else { header("location: index.php");
+} else { 
+    header("location: index.php");
     exit;
 }
 ?>
